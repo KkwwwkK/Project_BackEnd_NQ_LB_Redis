8149cb0924707981b83f895c9a755fb7d3291847
KkwwwkK <kenwky619@gmail.com>
2024-05-02 15:26:09 +0800

    trying stripe

--- src/main/java/com/fsse2401/project_backend/api/CheckOutApi.java
+++ src/main/java/com/fsse2401/project_backend/api/CheckOutApi.java
@@ -4,11 +4,13 @@ import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class CheckOutApi {
 //    @PostMapping("/public/checkout")
 //    public void checkout() throws StripeException {
